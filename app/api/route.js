341e29ee50aafa5fd4e26d6857c0e571e9b90ccc
veerathai1959 <veerathai@gmail.com>
2026-09-22 20:04:26 +0700

    Update route.js

--- app/api/route.js
+++ app/api/route.js
@@ -1,3 +1,3 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "Hello from Vercel!" });
+export async function GET() {
+  return Response.json({ message: "Hello from Vercel!" });
 }
